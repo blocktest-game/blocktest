e3bc885126e583f85c7c86bf6927abe58a7545ff
Mark Suckerberg <tommjd54321@outlook.com>
2021-03-14 17:00:41 -0500

    adds walk animation

--- Blocktest/Assets/Scripts/PlayerController.cs
+++ Blocktest/Assets/Scripts/PlayerController.cs
@@ -10,24 +10,40 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float jumpStrength = 7.5f;
     [SerializeField] LayerMask groundLayer;
     /// Rigidbody component of the player
-    private Rigidbody2D playerRB;
+    private Rigidbody2D playerRigidBody;
+    /// Animator component of the player
+    private Animator playerAnimator;
+    /// Sprite renderer component of the player
+    private SpriteRenderer playerSpriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        playerRB = GetComponent<Rigidbody2D>();
+        playerRigidBody = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+        playerSpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        if(horizontalInput != 0) 
-        {
-            playerRB.velocity = new Vector2(horizontalInput * moveSpeed, playerRB.velocity.y);
+        bool onGround = (playerRigidBody.GetContacts(new List<Collider2D>()) > 0); // Checks if the player is colliding with ANYTHING
+
+        playerAnimator.SetFloat("MoveSpeed", Mathf.Abs(horizontalInput));
+        playerAnimator.SetFloat("VerticalSpeed", playerRigidBody.velocity.y);
+
+        if(horizontalInput != 0){
+            playerRigidBody.velocity = new Vector2(horizontalInput * moveSpeed, playerRigidBody.velocity.y);
+
+            if(horizontalInput > 0) {
+                playerSpriteRenderer.flipX = false;
+            } else {
+                playerSpriteRenderer.flipX = true;
+            }
+
         }
-        if(Input.GetKeyDown(KeyCode.Space) && (playerRB.GetContacts(new List<Collider2D>()) > 0)) // Checks to see if the player is colliding with ANYTHING
-        {
-            playerRB.AddForce(Vector2.up * jumpStrength, ForceMode2D.Impulse);
+        if(Input.GetButtonDown("Jump") && onGround) {
+            playerRigidBody.AddForce(Vector2.up * jumpStrength, ForceMode2D.Impulse);
         }
     }
 }
