5430b22ea250f0dd1491773c1a29539e33e313aa
Yodeler <superjacob50@yahoo.com>
2021-03-29 22:50:39 -0400

    Improved jumping
    Jumping now only works if you are actually on the ground, also moved ground checking into its own function.

--- Blocktest/Assets/Scripts/PlayerController.cs
+++ Blocktest/Assets/Scripts/PlayerController.cs
@@ -7,8 +7,12 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float moveSpeed = 1;
     /// <summary> Jump strength of the player </summary>
     [SerializeField] private float jumpStrength = 7.5f;
+    /// <summary> Layer for touchable blocks </summary>
+    [SerializeField] private LayerMask groundLayer;
     /// <summary> Rigidbody component of the player </summary>
     private Rigidbody2D playerRigidBody;
+    /// <summary> Hitbox of the player </summary>
+    private BoxCollider2D playerHitbox;
     /// <summary> Animator component of the player </summary>
     private Animator playerAnimator;
     /// <summary> Sprite renderer component of the player </summary>
@@ -19,12 +23,12 @@ public class PlayerController : MonoBehaviour
         playerRigidBody = GetComponent<Rigidbody2D>();
         playerAnimator = GetComponent<Animator>();
         playerSpriteRenderer = GetComponent<SpriteRenderer>();
+        playerHitbox = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        bool onGround = playerRigidBody.GetContacts(new List<Collider2D>()) > 0; // Checks if the player is colliding with ANYTHING
 
         playerAnimator.SetFloat("MoveSpeed", Mathf.Abs(horizontalInput));
         playerAnimator.SetFloat("VerticalSpeed", playerRigidBody.velocity.y);
@@ -39,8 +43,20 @@ public class PlayerController : MonoBehaviour
             }
 
         }
-        if (Input.GetButtonDown("Jump") && onGround) {
+        if (Input.GetButtonDown("Jump") && onGround()) {
             playerRigidBody.AddForce(Vector2.up * jumpStrength, ForceMode2D.Impulse);
         }
+    }
+
+    private bool onGround()
+    {
+        /// Holds the center location of the ground detecting box, the center bottom of the hitbox
+        Vector2 groundBoxCenter = playerHitbox.bounds.center;
+        groundBoxCenter.y -= playerHitbox.bounds.extents.y;
+
+        Vector2 groundBoxSize = new Vector2(playerHitbox.bounds.size.x - 0.1f, 0.1f);
+
+        /// Check if the area directly under the player's hitbox is occupied by something in the groundlayer. Return true if yes, false if no.
+        return Physics2D.OverlapBox(groundBoxCenter, groundBoxSize, 0f, groundLayer) != null;
     }
 }
