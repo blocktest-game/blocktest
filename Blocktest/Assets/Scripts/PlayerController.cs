6698ae39483903ce5d1ff55ea60f0093ee875211
Yodeler <superjacob50@yahoo.com>
2021-04-01 20:48:31 -0400

    Readds walljumping, makes jumping more modular

--- Blocktest/Assets/Scripts/PlayerController.cs
+++ Blocktest/Assets/Scripts/PlayerController.cs
@@ -43,9 +43,29 @@ public class PlayerController : MonoBehaviour
             }
 
         }
-        if (Input.GetButtonDown("Jump") && onGround()) {
-            playerRigidBody.AddForce(Vector2.up * jumpStrength, ForceMode2D.Impulse);
+        if (Input.GetButtonDown("Jump")) {
+            tryJump();
         }
+    }
+
+    private void tryJump()
+    {
+        if (onGround()) {
+            groundJump();
+        }
+
+        else if (onWallRight()) {
+            groundJump();
+        }
+
+        else if (onWallLeft()) {
+            groundJump();
+        }
+    }
+
+    private void groundJump()       // Meant to be used for jumping while on the ground
+    {
+        playerRigidBody.AddForce(Vector2.up * jumpStrength, ForceMode2D.Impulse);
     }
 
     private bool onGround()
@@ -58,5 +78,41 @@ public class PlayerController : MonoBehaviour
 
         /// Check if the area directly under the player's hitbox is occupied by something in the groundlayer. Return true if yes, false if no.
         return Physics2D.OverlapBox(groundBoxCenter, groundBoxSize, 0f, groundLayer) != null;
+    }
+
+    private bool onCeiling()        // Bonk
+    {
+        /// Holds the center location of the ceiling detecting box, the center top of the hitbox
+        Vector2 groundBoxCenter = playerHitbox.bounds.center;
+        groundBoxCenter.y += playerHitbox.bounds.extents.y;
+
+        Vector2 groundBoxSize = new Vector2(playerHitbox.bounds.size.x - 0.1f, 0.1f);
+
+        /// Check if the area directly over the player's hitbox is occupied by something in the groundlayer. Return true if yes, false if no.
+        return Physics2D.OverlapBox(groundBoxCenter, groundBoxSize, 0f, groundLayer) != null;
+    }
+
+    private bool onWallRight()
+    {
+        /// Holds the center location of the right wall detecting box, the center right of the hitbox
+        Vector2 wallBoxCenter = playerHitbox.bounds.center;
+        wallBoxCenter.x += playerHitbox.bounds.extents.x;
+
+        Vector2 wallBoxSize = new Vector2(0.1f , playerHitbox.bounds.size.y - 0.1f);
+
+        /// Check if the area directly right of the player's hitbox is occupied by something in the groundlayer. Return true if yes, false if no.
+        return Physics2D.OverlapBox(wallBoxCenter, wallBoxSize, 0f, groundLayer) != null;
+    }
+
+    private bool onWallLeft()
+    {
+        /// Holds the center location of the left wall detecting box, the center bottom of the hitbox
+        Vector2 wallBoxCenter = playerHitbox.bounds.center;
+        wallBoxCenter.x -= playerHitbox.bounds.extents.x;
+
+        Vector2 wallBoxSize = new Vector2(0.1f , playerHitbox.bounds.size.y - 0.1f);
+
+        /// Check if the area directly left of the player's hitbox is occupied by something in the groundlayer. Return true if yes, false if no.
+        return Physics2D.OverlapBox(wallBoxCenter, wallBoxSize, 0f, groundLayer) != null;
     }
 }
