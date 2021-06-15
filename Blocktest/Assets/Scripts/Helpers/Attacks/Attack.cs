ae56607b822664f9365d32b79b4bdbb7ec37312b
Yodeler <superjacob50@yahoo.com>
2021-06-15 17:39:06 -0400

    Adds basic attack support
    Adds Attackable interface and Attack struct

---
+++ Blocktest/Assets/Scripts/Helpers/Attacks/Attack.cs
@@ -0,0 +1,15 @@
+/// <summary> Stores information on an attacks effect on an attackable object </summary>
+struct Attack
+{
+    /// <summary> Amount of damage dealt </summary>
+    public int damage;
+
+    /// <summary> Type of damage dealt </summary>
+    public byte type;
+
+    public Attack(int newDamage, byte newType)
+    {
+        damage = newDamage;
+        type = newType;
+    }
+}
---
+++ Blocktest/Assets/Scripts/Helpers/Attacks/Attackable.cs
@@ -0,0 +1,6 @@
+/// <summary> Handles incoming attacks </summary>
+interface Attackable
+{
+    /// <summary> Handles being attacked </summary>
+    public void recieveAttack(Attack hit);
+}
