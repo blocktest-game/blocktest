e146fb5639f29dec7b014b495af26f037a082744
Yodeler <superjacob50@yahoo.com>
2021-06-16 04:35:25 -0400

    Adds potentially helpful comment

--- Blocktest/Assets/Scripts/Helpers/Attacks/Attackable.cs
+++ Blocktest/Assets/Scripts/Helpers/Attacks/Attackable.cs
@@ -1,4 +1,5 @@
 /// <summary> Handles incoming attacks </summary>
+/// <remarks> You can test for this with <code>if(class is Attackable)</code> </remarks>
 interface Attackable
 {
     /// <summary> Handles being attacked </summary>
